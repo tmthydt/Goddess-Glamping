0fb35854e4fc1d5273d5d33ee624eafcb360b0e4
Heather Wenzel <hcwenzel@gmail.com>
2019-05-09 16:25:04 -0400

    first push to commit to master

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
 "use strict";
 
 // Get the modal
-var modal = document.getElementById('myModal');
+var liveModal = document.getElementById('liveModal');
+
+// Get the button that opens the modal
+var liveBtn = document.getElementById("liveBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the modal, open the modal 
-modal.onclick = function() {
-  modal.style.display = "block";
+// When the user clicks the button, open the modal 
+liveBtn.onclick = function() {
+    liveModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    liveModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == liveModalClass) {
+        liveModal.style.display = "none";
+    }
 }
+
+// Get the modal
+var laughModal = document.getElementById('laughModal');
+
+// Get the button that opens the modal
+var laughBtn = document.getElementById("laughBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+laughBtn.onclick = function() {
+    laughModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    laughModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+        if (event.target == lauodal) {
+            laughModal.style.display = "none";
+        }
