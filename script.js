6997a1ac38411a248b00a92362f11f0cce0668d6
Heather Wenzel <hcwenzel@gmail.com>
2019-05-21 21:53:45 -0400

    first commit for new GitHub repo

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//this function closes all of the checkout page popups
 function closeModals(event) {
   if (event.target.classList.contains("closeModal")) {
     let modal1 = document.querySelector(".cart__modal");
@@ -13,17 +14,20 @@ function closeModals(event) {
   }
 }
 
+//this is the query selector that selects the main element and adds an event listener that runs the closeModals function when clicked
 document.querySelector("main").addEventListener("click", closeModals);
 
+//this is the query selector that selects the entire container for all of the categories, and adds an event  listener that toggles the hidden class when an element with the class container__title is clicked
 document.querySelector(".item__container").addEventListener("click", function (event) {
   if (event.target.classList.contains("container__title")) {
     event.target.nextElementSibling.classList.toggle("hidden");
   }
 });
 
-
+//this is the query selector that selects the shopping cart icon and adds an event listener that runs the showCart function when clicked
 document.querySelector(".material-icons").addEventListener("click", showCart);
 
+//this is the function that shows the cart popup, fades out the background, and hides the list of each of the categories
 function showCart() {
   document.querySelector(".cart__modal").style.display = "block";
   document.querySelector(".cart__container").style.display = "block";
@@ -32,16 +36,20 @@ function showCart() {
   document.querySelector(".lounge__list").classList.add("hidden");
 }
 
+//this query selector selects the "checkout" button on the cart popup, and adds an event listener that will run the showCheckout function when clicked
 document.querySelector(".checkout-button").addEventListener("click", showCheckout);
 
+//this function shows the checkout popup, hides the previous cart popup, and adds content to the popup that shows the total price
 function showCheckout() {
   document.querySelector(".checkout-page").style.display = "block";
   document.querySelector(".cart__container").style.display = "none";
   document.querySelector("p.priceOfCart").innerHTML = `Your total is $${total.toFixed(2)}`;
 }
 
+//this query selector selects the "submit" button on the checkout popup, and adds an event listener that will run the showReceipt function when clicked
 document.querySelector(".checkout-submit").addEventListener("click", showReceipt);
 
+//this function shows the receipt popup, hides the previous checkout popup, and adds content to the popup that shows all of the items purchased, the subtotal, and the total price. If the user pays more cash than the cost of the purchase, this function also adds content that says how much is owed to the user
 function showReceipt() {
   document.querySelector(".receipt-page").style.display = "block";
   document.querySelector(".checkout-page").style.display = "none";
@@ -68,7 +76,7 @@ function showReceipt() {
 
 
 
-
+//this declares three variables, setting the total to start at 0, setting the shoppingCart to an empty array, and setting the itemList to an array of 10 items, with their corresponding price values
 let total = 0.;
 let shoppingCart = [];
 let itemList = [
@@ -84,14 +92,21 @@ let itemList = [
   { name: "Tower Fan", price: 500 }
 ];
 
-const addItem = (newItem) => shoppingCart = [...shoppingCart, { ...newItem }];
+//this function declares a variable called addItem, which accepts a parameter of newItem, re-initializes shoppingCart to be equal to a copy of the shopping cart (using spread), and adds a copy of newItem to the end of the shoppingCart array
+// const addItem = (newItem) => shoppingCart = [...shoppingCart, { ...newItem }];
+function addItem(newItem) {
+  shoppingCart = [...shoppingCart, { ...newItem }];
+}
 
+//this function declares a variable called removeItem, which accepts a parameter of itemName, declares a variable called index, which searches through the shopping cart to find the item name, and if it matches, removes it, and updates the total
 const removeItem = itemName => {
   const index = shoppingCart.findIndex(item => item.name === itemName);
   shoppingCart = [...shoppingCart.slice(0, index), ...shoppingCart.slice(index + 1)];
   updateTotal();
 }
 
+
+//this function sets the total to 0, then to the sum of each item added up. The HTML formats/displays the subtotal, total and total w/tax separately in the cart__total element
 const updateTotal = () => {
   total = 0;
   for (let item of shoppingCart) { total += item.price; }
@@ -102,8 +117,10 @@ const updateTotal = () => {
   `;
 }
 
+//this declares a variable called main that is the selection of the main element in the DOM
 const main = document.querySelector("main");
 
+//we add a click event listener to the main element, which runs an anonymous function that does two things: (1) If the class of the target event contains "add__btn", it runs the addItem function, runs the display function, and runs the updateTotal function. (2) Otherwise, if the target event contains "remove__btn", it runs the removeItem function, removes the item from the target event's parentNode, and runs the updateTotal function
 main.addEventListener("click", function (event) {
   if (event.target.classList.contains("add__btn")) {
     addItem(itemList[event.target.attributes[0].value]);
@@ -116,6 +133,7 @@ main.addEventListener("click", function (event) {
   }
 });
 
+//this function has a parameter of item, declares an variable that creates a new li element, changes the innerHTML, and appends the element to the selected cart__list item
 function display(item) {
   const el = document.createElement("li");
   el.innerHTML = `
@@ -125,6 +143,7 @@ function display(item) {
   document.querySelector("ul.cart__list").append(el);
 }
 
+//this for loop goes through each item in the itemList, and if it matches, creates a new li element that is appended to the selected ul item, and changes the innerHTML of the element
 for (let index in itemList) {
   if (itemList[index].name === "Armand de Brignac Champagne") {
     const itemUL = document.querySelector("ul.live__list");
